c14055fd343a1d589400b94a794f6241dcab2d31
Pedro de Mesquita Bittencourt <djojoo@gmail.com>
2022-05-17 00:00:00 -0300

    commit20220517

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,23 @@
+use gloo::console::log;
+use serde::Serialize;
 use yew::prelude::*;
+
+#[derive(Serialize)]
+struct MyStruct {
+    first_name: String,
+    last_name: String,
+}
 
 #[function_component(App)]
 pub fn app() -> Html {
+    let name: &str = "Pedro";
+    let my_struct: MyStruct = MyStruct {
+        first_name: name.to_owned(),
+        last_name: "Bittencourt".to_string(),
+    };
+
+    log!(name);
+    log!(serde_json::to_string_pretty(&my_struct).unwrap());
     html! {
         <h1>{"Hello World!"}</h1>
     }
