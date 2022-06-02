c68a92dd4494359a91545eb878a846bee01fb911
Pedro de Mesquita Bittencourt <djojoo@gmail.com>
2022-06-02 00:00:00 -0300

    limpando testes e organizando estrutura20220602

--- src/lib.rs
+++ src/lib.rs
@@ -1,56 +1,18 @@
-use gloo::console::log;
-use serde::Serialize;
-use stylist::{style, yew::styled_component, Style};
+use stylist::{yew::styled_component, Style};
 use yew::prelude::*;
 
-#[derive(Serialize)]
-struct MyStruct {
-    first_name: String,
-    last_name: String,
-}
+mod components;
+
+use components::main_title::MainTitle;
+
+const STYLESHEE_FILE: &str = include_str!("style.css");
+
 #[styled_component(App)]
 pub fn app() -> Html {
-    let name: &str = "Pedro";
-    let my_struct: MyStruct = MyStruct {
-        first_name: name.to_owned(),
-        last_name: "Bittencourt".to_string(),
-    };
-
-    log!(name);
-    log!(serde_json::to_string_pretty(&my_struct).unwrap());
-
-    let message: Option<&str> = Some("Mensagem");
-
-    let to_do_list = vec![
-        "ir no mercado",
-        "comprar cerveja",
-        "pedir um X",
-        "assistir aula",
-    ];
-
-    const STYLESHEE_FILE: &str = include_str!("style.css");
     let stylesheet: Style = Style::new(STYLESHEE_FILE).unwrap();
     html! {
-        <>
         <div class={stylesheet}>
-            <h1>{"Hello World!"}</h1>
-
-            if let Some(message) = message {
-                <p>{message}</p>
-            } else {
-                <p>{"sem mensagem"}</p>
-            }
-
-            <ul>
-            // {to_do_list.iter().map(|item| html!{<li>{item}</li>}).collect::<Html>()}
-                {to_li_html(to_do_list)}
-            </ul>
+            <MainTitle title="Olá  mundo!"/>
         </div>
-
-        </>
     }
-}
-
-fn to_li_html(list: Vec<&str>) -> Vec<Html> {
-    list.iter().map(|item| html! {<li>{item}</li>}).collect()
 }
