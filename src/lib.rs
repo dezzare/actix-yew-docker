56972abf138acb292637ff4f6ede108520ce8929
Pedro de Mesquita Bittencourt <djojoo@gmail.com>
2022-06-03 00:00:00 -0300

    pequeno avanço na organização e entendimento do uso de styles20220603

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use yew::prelude::*;
 
 mod components;
 
-use components::main_title::MainTitle;
+use components::main_title::{Color, MainTitle};
 
 const STYLESHEE_FILE: &str = include_str!("style.css");
 
@@ -12,7 +12,7 @@ pub fn app() -> Html {
     let stylesheet: Style = Style::new(STYLESHEE_FILE).unwrap();
     html! {
         <div class={stylesheet}>
-            <MainTitle title="Olá  mundo!"/>
+            <MainTitle title="Olá  mundo!" color={Color::Ok} />
         </div>
     }
 }
